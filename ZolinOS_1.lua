61f4a6c39e96fd0bbd74cf8c6d4989636c113d6c
Sky Attacker <75571293+mohammadbakon123x@users.noreply.github.com>
2026-06-07 05:39:24 +0300

    ZolinOS Loader

---
+++ ZolinOS_1.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mohammadbakon123x/Zolin-OS/refs/heads/main/Initiator.lua"))();
+task.wait();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mohammadbakon123x/Zolin-OS/refs/heads/main/ZolinModules.lua"))();
