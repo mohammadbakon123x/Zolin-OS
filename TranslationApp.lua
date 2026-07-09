71d6fb490496f761b918cb39a9f4c0aa94f7d838
Sky Attacker <75571293+mohammadbakon123x@users.noreply.github.com>
2026-07-09 19:14:34 +0300

    Update TranslationApp.lua

--- TranslationApp.lua
+++ TranslationApp.lua
@@ -3,7 +3,7 @@ function TranslationApp.Init(ui, launchArgs, appFolder)
 	local l__TweenService__5 = game:GetService("TweenService");
 	local UIS = game:GetService("UserInputService");
 	local u6 = game:GetService("RunService")
-	local BuildVersion = "3.21.4"
+	local BuildVersion = "3.21.5"
 	local versionLabel = "v"..BuildVersion;
 	local SettingsScript = {
 		RequireAway = false,
