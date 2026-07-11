d547f9354b314bb30e092c55240d5370f2850729
Sky Attacker <75571293+mohammadbakon123x@users.noreply.github.com>
2026-07-11 07:15:55 +0300

    Update ZolinOS_1.lua

--- ZolinOS_1.lua
+++ ZolinOS_1.lua
@@ -1,3 +1,4 @@
+if game:GetService("RunService"):IsStudio() then warn("ZolinOS is not supported in Studio.") return end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mohammadbakon123x/Zolin-OS/refs/heads/main/Initiator.lua"))();
 task.wait();
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mohammadbakon123x/Zolin-OS/refs/heads/main/ZolinModules.lua"))();
