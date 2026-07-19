e9ad392716d7a5f009d802a2c7a08fda4b4d5c45
Sky Attacker <75571293+mohammadbakon123x@users.noreply.github.com>
2026-07-19 21:17:50 +0300

    Update Initiator.lua

--- Initiator.lua
+++ Initiator.lua
@@ -1,5 +1,5 @@
 local v1 = {};
-v1.ver = "1.3.7" -- versionOS
+v1.ver = "1.3.8" -- versionOS
 
 -- ============================================
 -- HELPER FUNCTIONS
@@ -2840,8 +2840,6 @@ local function createChunk25()
 	UIShadow.Parent = SelectionButton;
 	UIShadow.Color = Color3.fromRGB(255, 255, 255);
 	UIShadow.BlurRadius = UDim.new(0, 20);
-	UIShadow.Offset = UDim2.new(0, 0, 0, 0);
-	UIShadow.Spread = UDim2.new(0, 0, 0, 0);
 	UIShadow.Transparency = 0.5;
 	UIShadow.ZIndex  = -1;
 end
