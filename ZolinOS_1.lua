00dcbe9c70a4d0da4522fc8ae77d0aed23f0ce37
Sky Attacker <75571293+mohammadbakon123x@users.noreply.github.com>
2026-07-19 20:02:34 +0300

    Modify ZolinOS loading process to include firmware
    Updated the loading sequence to include ZolinFirmware and added a wait after each load.

--- ZolinOS_1.lua
+++ ZolinOS_1.lua
@@ -6,4 +6,6 @@ if ZolinOS then
 end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mohammadbakon123x/Zolin-OS/refs/heads/main/Initiator.lua"))();
 task.wait();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mohammadbakon123x/Zolin-OS/refs/heads/main/ZolinFirmware.lua"))()
+task.wait();
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mohammadbakon123x/Zolin-OS/refs/heads/main/ZolinModules.lua"))();
